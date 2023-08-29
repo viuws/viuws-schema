671ab026378f433457388bffa009ed9ec2354c7e
Jonas Windhager <jonas.windhager@uzh.ch>
2023-08-30 01:06:18 +0200

    registry update

--- viuws_schema/v01/registry.py
+++ viuws_schema/v01/registry.py
@@ -1,5 +1,11 @@
+from ..base import SchemaBaseModel
 from .base import RootSchemaBaseModelV01
 
 
+class ProcessRef(SchemaBaseModel):
+    id: str
+    path: str
+
+
 class Registry(RootSchemaBaseModelV01):
-    processes: dict[str, str] = {}
+    processes: list[ProcessRef] = []
