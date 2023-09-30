b72c33245e91be750d4ccb54367d36e290b74324
Jonas Windhager <jonas.windhager@scilifelab.se>
2023-09-30 16:13:32 +0200

    add basedir to generate-site.sh

--- generate-site.sh
+++ generate-site.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; pwd -P )
 for version in $(viuws-schema versions); do
-    rm -rf _site/${version}
-    mkdir -p _site/${version}
+    rm -rf ${basedir}/_site/${version}
+    mkdir -p ${basedir}/_site/${version}
     for model in $(viuws-schema models --version ${version}); do
-        viuws-schema generate --version ${version} ${model} > _site/${version}/${model,,}.json
+        viuws-schema generate --version ${version} ${model} > ${basedir}/_site/${version}/${model,,}.json
     done
-    generate-schema-doc --expand-buttons --no-link-to-reused-ref --deprecated-from-description _site/${version} _site/${version}
+    generate-schema-doc --expand-buttons --no-link-to-reused-ref --deprecated-from-description ${basedir}/_site/${version} ${basedir}/_site/${version}
 done
