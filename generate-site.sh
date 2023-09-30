75fc68f24728f6787b3776d4386714f2b85e53c8
Jonas Windhager <jonas.windhager@scilifelab.se>
2023-09-30 17:23:53 +0200

    remove basedir (WSL compatibility)

--- generate-site.sh
+++ generate-site.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; pwd -P )
 for version in $(viuws-schema versions); do
-    rm -rf "${basedir}/_site/${version}"
-    mkdir -p "${basedir}/_site/${version}"
-    for model in $(viuws-schema models --version ${version}); do
-        viuws-schema generate --version ${version} ${model} > "${basedir}/_site/${version}/${model,,}.json"
+    rm -rf "_site/${version}"
+    mkdir -p "_site/${version}"
+    for model in $(viuws-schema models --version "${version}"); do
+        viuws-schema generate --version "${version}" "${model}" > "_site/${version}/${model,,}.json"
     done
-    generate-schema-doc --expand-buttons --no-link-to-reused-ref --deprecated-from-description "${basedir}/_site/${version}" "${basedir}/_site/${version}"
+    generate-schema-doc --expand-buttons --no-link-to-reused-ref --deprecated-from-description "_site/${version}" "_site/${version}"
 done
