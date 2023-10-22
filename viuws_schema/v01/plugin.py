2af845cf2424e4c4c47a0247e9e3750b6d173632
Jonas Windhager <jonas.windhager@scilifelab.se>
2023-10-22 10:48:15 +0200

    explicit JSFunction type

--- viuws_schema/v01/plugin.py
+++ viuws_schema/v01/plugin.py
@@ -6,6 +6,8 @@ from pydantic import Field
 from ..base import SchemaBaseModel
 from .base import RootSchemaBaseModelV01
 
+JSFunction = Any
+
 
 class PluginType(str, Enum):
     IMPORT = "import"
@@ -20,13 +22,13 @@ class PluginBase(SchemaBaseModel):
 class ImportPlugin(PluginBase):
     type: Literal[PluginType.IMPORT]
     import_menu_item: str = Field(alias="importMenuItem")
-    import_function: Any = Field(alias="importFunction")
+    import_function: JSFunction = Field(alias="importFunction")
 
 
 class ExportPlugin(PluginBase):
     type: Literal[PluginType.EXPORT]
     export_menu_item: str = Field(alias="exportMenuItem")
-    export_function: Any = Field(alias="exportFunction")
+    export_function: JSFunction = Field(alias="exportFunction")
 
 
 class Plugin(RootSchemaBaseModelV01):
